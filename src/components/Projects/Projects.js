1cf97c729b8a87e6f8c3cfddfd7c15a703896c65
jonasmachados <jonasmachado.ti@gmail.com>
2022-07-27 18:18:08 -0300

    Create Project component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,92 @@
+import { Col, Container, Row, Tab, Nav } from "react-bootstrap";
+import ProjectCard from "../ProjectsCard/ProjectCard";
+import projImg1 from "../../assets/img/project-img1.png";
+import projImg2 from "../../assets/img/project-img1.png";
+import projImg3 from "../../assets/img/project-img1.png";
+import "./Projects.css";
+import "../../App.css";
+
 const Projects = () => {
-    return (
-      <div>
-        <h1>Projects</h1>  
-      </div>
-    );
-  }
-  
-  export default Projects;
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects I Built!</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav variant="pills" defaultActiveKey="/home">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab One</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+
+                <Tab.Pane eventKey="second">
+                  <p>Lorem ipsum</p>
+                </Tab.Pane>
+
+                <Tab.Pane eventKey="third">
+                  <p>Lorem ipsum</p>
+                </Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Projects;
