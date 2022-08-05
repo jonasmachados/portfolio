d49197162d5f457ac1b49d13c3b50f24e4712f41
jonasmachados <jonasmachado.ti@gmail.com>
2022-08-05 15:17:28 -0300

    style projects cards

--- src/components/ProjectsCard/ProjectCard.js
+++ src/components/ProjectsCard/ProjectCard.js
@@ -3,6 +3,7 @@ import "../Projects/Projects.css";
 
 const ProjectCard = ({ title, description, imgUrl }) => {
   return (
+    /*
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} />
@@ -12,6 +13,16 @@ const ProjectCard = ({ title, description, imgUrl }) => {
         </div>
       </div>
     </Col>
+    */
+    <div class="container-project">
+      <div class="middle">
+        <div class="text">
+          <h4>{title}</h4>
+          <span>{description}</span>
+        </div>
+      </div>
+      <img src={imgUrl} alt="Avatar" class="image" />
+    </div>
   );
 };
 
