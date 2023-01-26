63e95b41abc95b77db5bde04197a0b16be1f7fd0
jonasmachados <jonasmachado.ti@gmail.com>
2023-01-25 21:37:46 -0300

    Add Animation To Banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,11 +1,62 @@
 import { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { motion } from 'framer-motion';
 import headerImg from "../../assets/img/dev.svg";
-import { ArrowRightCircle } from "react-bootstrap-icons";
 import './Banner.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Pdf from "../../assets/pdf/Curriculo.pdf";
 
+const buttonVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: { delay: 4.5 }
+  },
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px var(--primary-color)",
+    boxShadow: "0px 0px 8px var(--primary-color)",
+    transition: {
+      duration: 0.3,
+      repeatType: "mirror",
+      repeat: Infinity
+    }
+  }
+}
+
+const bannerVariants = {
+  hidden: {
+    scale: 0.9
+  },
+  visible: {
+    scale: 1,
+    transition: { duration: 1.3 },
+  },
+  exit: {
+    x: "-100vh",
+    transition: { ease: 'easeInOut' }
+  }
+};
+
+const listVariants = {
+  hidden: { scale: 0 },
+  visible: {
+    scale: 1,
+    transition: {
+      delayChildren: 1.8,
+      staggerChildren: 1
+    }
+  }
+};
+
+const item = {
+  hidden: {  scale: 0 },
+  visible: {
+    scale: 1,
+    paddingBottom: 30
+  }
+};
 
 const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
@@ -23,7 +74,7 @@ const Banner = () => {
     return () => {
       clearInterval(ticker);
     };
-      // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [text]);
 
   const tick = () => {
@@ -49,35 +100,63 @@ const Banner = () => {
     }
   };
 
-  return (
-    <section className="banner" id="home">
-      <Container>
-        <Row className="aligh-items-center">
-          <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Welcome to my Portfolio</span>
-            <h1>
-              {" "}
-              {`Hi! I'm Jonas`}<br></br>{" "}
-              <span
-                className="txt-rotate"
-              >
-                <span className="wrap">{text}</span>
-              </span>
-            </h1>
-            <h2>Developer | React | SpringBoot</h2>
-            <p>🌱 I’m currently learning web development (NodeJS, ReactJS and SpringBoot).</p>
-            <p>🧑‍💻 I’am looking for new job opportunities to Dev Full Stack Springboot | React.</p>
-            <button onClick={() => window.open(Pdf) & console.log("connect")}>
-              Resume <ArrowRightCircle size={25} />
-            </button>
+  const listInfo = ["🌱 I’m currently learning web development (NodeJS, ReactJS and SpringBoot).",
+    "🧑‍💻 I’am looking for new job opportunities to Dev Full Stack Springboot | React."]
 
-          </Col>
-          <Col xs={12} md={6} xl={5}>
-            <img src={headerImg} alt="Header Img" />
-          </Col>
-        </Row>
-      </Container>
-    </section>
+  return (
+    <div className="banner" >
+
+      <motion.div
+        variants={bannerVariants}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+        <span className="tagline">Welcome to my Portfolio</span>
+        <h1>
+          {" "}
+          {`Hi! I'm Jonas`}<br></br>{" "}
+          <span
+            className="txt-rotate"
+          >
+            <span className="wrap">{text}</span>
+          </span>
+        </h1>
+        <h2>Developer | React | SpringBoot</h2>
+
+        <motion.ul className="listVariants"
+          variants={listVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          {listInfo.map((index) => {
+            return (
+              <>
+                <motion.li
+                  variants={item}
+                >
+                  {index}
+                </motion.li>
+              </>
+            );
+          })}
+        </motion.ul>
+
+        <motion.button
+          variants={buttonVariants}
+          initial="hidden"
+          animate="visible"
+          whileHover="hover"
+          onClick={() => window.open(Pdf) &
+            console.log("connect")}> Resume
+        </motion.button>
+
+      </motion.div>
+
+      <div>
+        <img src={headerImg} alt="Header Img" />
+      </div>
+    </div>
   );
 };
 
