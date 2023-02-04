6019bfc320ef67555cf583190d80d07b833ea1c6
jonasmachados <jonasmachado.ti@gmail.com>
2023-02-04 15:01:33 -0300

    Alter Styling AboutCOmponent

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
-import { IconContext } from "react-icons";
-import { FiPlus, FiMinus } from "react-icons/fi";
 import "./Quiz.css"
 import { motion } from 'framer-motion';
+import Faq from "react-faq-component";
+
 const containerVariants = {
   hidden: {
     opacity: 0,
@@ -17,60 +16,55 @@ const containerVariants = {
   }
 };
 
+const data = {
+  rows: [
+    {
+      title: <p className="p_title">Who am I ?</p>,
+      content: <p className="p_content">My name is Jonas Machado, i am 32 years old and born in Brazil - MG, currently i am looking for new job opportunities as a developer.</p>,
+    },
+    {
+      title: <p className="p_title">What I do ? </p>,
+      content:
+        <p className="p_content">I am interested in Web development, i develop web site using in the back-end Java and fron-tend React.</p>,
+    },
+    {
+      title: <p className="p_title">What is my educational background ?</p>,
+      content: <p className="p_content">I have a degree in Information Systems from Universitas.</p>,
+    },
+  ],
+};
+
+const styles = {
+  titleTextColor: 'blue',
+  bgColor: 'var(--primary-color)',
+  arrowColor: "#fff",
+};
+
+const config = {
+  animate: true,
+  arrowIcon: "V",
+  tabFocus: true,
+  openOnload: 0,
+  expandIcon: "+",
+  collapseIcon: "-",
+};
+
 const Quiz = () => {
-  const Data = [
-    {
-      question: 'Who am I ?',
-      answer: 'My name is Jonas Machado, i am 32 years old and born in Brazil - MG, currently i am looking for new job opportunities as a developer.'
-    },
-    {
-      question: 'What I do ?',
-      answer: 'I am interested in Web development, i develop web site using in the back-end Java and fron-tend React. '
-    },
-    {
-      question: 'What is my educational background ?',
-      answer: ' I have a degree in Information Systems from Universitas. '
-    },
-  ];
-
-  const [clicked, setClicked] = useState(false);
-
-  const toggle = (index) => {
-    if (clicked === index) {
-      //if clicked question is already active, then close it
-      return setClicked(null);
-    }
-
-    setClicked(index);
-  };
 
   return (
-    <IconContext.Provider value={{ color: "var(--primary-color)", size: "35px" }}>
-      <motion.div className="accordionSection"
-        variants={containerVariants}
-        initial="hidden"
-        whileInView="visible"
-        exit="exit">
-        <div className="box">
-          {Data.map((item, index) => {
-            return (
-              <div key={index}>
-                <div className="wrap" onClick={() => toggle(index)} key={index}>
-                  <h2>{item.question}</h2>
-                  <span>{clicked === index ? <FiMinus /> : <FiPlus />}</span>
-                </div>
-                {clicked === index ? (
-                  <div className="dropdown">
-                    <p>{item.answer}</p>
-                  </div>
-                ) : null}
-              </div>
-            );
-          })}
-        </div>
-      </motion.div>
-    </IconContext.Provider>
+    <motion.div className="container"
+      variants={containerVariants}
+      initial="hidden"
+      whileInView="visible"
+      exit="exit">
+      <Faq
+        data={data}
+        styles={styles}
+        config={config}
+      />
+    </motion.div>
+
   );
-};
+}
 
 export default Quiz;
