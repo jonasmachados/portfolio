807a71e8c61d491372516d81e26ec47c1c7e166e
jonasmachados <jonasmachado.ti@gmail.com>
2023-02-07 14:26:47 -0300

    Styling Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,38 +1,32 @@
-import { Container, Row, Col } from "react-bootstrap";
-
 import logo from "../../assets/img/logo.png";
 import "./Footer.css";
-
 import { FaLinkedinIn } from 'react-icons/fa';
 import { AiFillInstagram, AiFillGithub } from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <Container>
-        <Row className="align-items-center">
-          <Col size={12} sm={6}>
-            <div className="img-logo">
-              <img src={logo} alt="Logo" />
-            </div>
-          </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="https://github.com/jonasmachados">
-                <i><AiFillGithub /></i>
-              </a>
-              <a href="https://www.linkedin.com/in/jonas-machados/">
-                <i><FaLinkedinIn /></i>
-              </a>
-              <a href="https://www.instagram.com/jonasmachado01/">
-                <i> <AiFillInstagram /></i>
-              </a>
-            </div>
-            <p>Copyright 2022. All Rights Reserved</p>
-          </Col>
-        </Row>
-      </Container>
-    </footer>
+    <div className="footer">
+      <div className="img-logo">
+        <img src={logo} alt="Logo" />
+      </div>
+
+      <div className="social-media">
+        <div className="social-icon">
+          <a href="https://github.com/jonasmachados">
+            <i><AiFillGithub /></i>
+          </a>
+          <a href="https://www.linkedin.com/in/jonas-machados/">
+            <i><FaLinkedinIn /></i>
+          </a>
+          <a href="https://www.instagram.com/jonasmachado01/">
+            <i> <AiFillInstagram /></i>
+          </a>
+        </div>
+        <p className="p-social-icon">Copyright 2023. All Rights Reserved</p>
+      </div>
+
+
+    </div>
   );
 };
 
