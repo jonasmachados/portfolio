72610a05441f096073b7237af0da1caf229c29d4
Jonas Machado <jonasmachado.ti@gmail.com>
2023-11-22 22:26:33 -0300

    :sparkles: feat: Create Header.js component for improved project structure.

---
+++ src/components/Header.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import { motion } from 'framer-motion';
+import {
+  FaBars,
+  FaTimes,
+  FaLinkedin,
+  FaGithub,
+  FaInstagram
+} from "react-icons/fa";
+import {
+  AiFillHome,
+  AiFillInfoCircle,
+  AiFillProject,
+  AiFillContacts
+} from 'react-icons/ai';
+import { GiSkills } from 'react-icons/gi';
+import { BiChevronDown } from 'react-icons/bi';
+import { HiDocumentReport } from 'react-icons/hi';
+import "../styles/header.css";
+import logo from "../assets/img/logo.png";
+
+const Header = () => {
+
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => {
+    setClick(!click);
+
+    document.body.style.overflow = !click ? 'hidden' : 'auto';
+  };
+
+  return (
+    <>
+      <div className="header-container">
+
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+        </div>
+
+        <motion.div
+          className="menu-toggle-icon"
+          onClick={handleClick}
+          initial={{ y: -200 }}
+          animate={{ y: 0 }}
+          transition={{ delay: 0.2, type: 'spring', stiffness: 120 }}
+        >
+          {click ? (
+            <FaTimes />
+          ) : (
+            <FaBars />
+          )}
+        </motion.div>
+
+        <ul className={`nav-menu ${click ? "active" : ""}`}>
+          <li>
+            <a href="/#">
+              <AiFillHome className="nav-menu-icon" />
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a href="/#about">
+              <AiFillInfoCircle className="nav-menu-icon" />
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="/#skills">
+              <GiSkills className="nav-menu-icon" />
+              Skills
+            </a>
+          </li>
+
+          <li>
+            <a href="/#project">
+              <AiFillProject className="nav-menu-icon" />
+              Projects
+            </a>
+          </li>
+
+          <li>
+            <a href="/#contact">
+              <AiFillContacts className="nav-menu-icon" />
+              Contact
+            </a>
+          </li>
+
+          <li className="sub-meunu-li">
+            <a href={`/`}>
+              <HiDocumentReport className="nav-menu-icon" />
+              Social Media
+              <BiChevronDown className="nav-menu-icon" />
+            </a>
+
+            <ul className="sub-menu">
+              <li>
+                <a href="https://www.linkedin.com/in/jonas-machados/"
+                  rel="noreferrer"
+                  target="_blank">
+                  <FaLinkedin className="nav-menu-icon" />
+                  LinkedIn
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://github.com/jonasmachados"
+                  rel="noreferrer"
+                  target="_blank">
+                  <FaGithub className="nav-menu-icon" />
+                  Github
+                </a>
+              </li>
+
+              <li>
+                <a href="https://www.instagram.com/jonasmachado01/"
+                  rel="noreferrer"
+                  target="_blank">
+                  <FaInstagram className="nav-menu-icon" />
+                  Instagram
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+
+      </div>
+
+      <div class="spacer"></div>
+    </>
+  );
+};
+
+export default Header;
