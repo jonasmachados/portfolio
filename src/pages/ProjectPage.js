27f4fd4656eea76fd36f77e3be35e7f788afa248
Jonas Machado <jonasmachado.ti@gmail.com>
2024-01-09 16:26:31 -0300

    :recycle: refactor: restructure project for ProjectPage
    - Refactored project structure by removing ProjectPage.js, styles.css, and the Pages folder from src/components.
    - Introduced a new src/pages folder and added ProjectPage component.
    - Created project-page.css for styling.

---
+++ src/pages/ProjectPage.js
@@ -0,0 +1,77 @@
+import { useParams } from "react-router-dom";
+import ProjectsData from "../data/ProjectsData.js";
+import { IoOpenOutline } from "react-icons/io5";
+import { IoLogoGithub } from "react-icons/io";
+import Error404 from "../components/Error/Error404.js";
+import "../styles/project-page.css";
+import "../styles/page.css";
+
+const ProjectPage = () => {
+  const { id } = useParams();
+
+  const project = ProjectsData.find((project) => project.id === Number(id));
+
+  if (!project) {
+    console.log("No project found with the provided id");
+    return <Error404 message="No project found with the provided id" />;
+  }
+
+  const {
+    title,
+    p_card1,
+    p_card2,
+    p_card3,
+    imgUrl,
+    linkSite,
+    linkGit,
+    description,
+  } = project;
+
+  const descriptionParagraphs = description.split("\n");
+
+  return (
+    <div className="container-project-page">
+      <h1>{title}</h1>
+
+      <img src={imgUrl} alt="img-project" />
+
+      <div className="container-cards">
+        <h2 className="card-item">{p_card1}</h2>
+        <h2 className="card-item">{p_card2}</h2>
+        <h2 className="card-item">{p_card3}</h2>
+      </div>
+
+      {linkSite && (
+        <a href={linkSite}>
+          <p>
+            Open Website <IoOpenOutline/>
+          </p>
+        </a>
+      )}
+
+      <div className="description">
+        {descriptionParagraphs.map((paragraph, index) => (
+          <p key={index}>{paragraph.trim()}</p>
+        ))}
+      </div>
+
+      {linkSite && (
+        <a href={linkSite}>
+          <p>
+            {" "}
+            Open Website <IoOpenOutline/>
+          </p>
+        </a>
+      )}
+
+      <a href={linkGit}>
+        <p>
+          Open GitHub <IoLogoGithub/>
+        </p>
+      </a>
+    </div>
+  );
+  
+};
+
+export default ProjectPage;
