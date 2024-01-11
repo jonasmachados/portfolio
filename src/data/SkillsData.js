c1a88163f7bdf5c22f55096fc21a36426116a8a5
Jonas Machado <jonasmachado.ti@gmail.com>
2024-01-11 17:44:49 -0300

    :card_file_box: raw: Update skills date and icon in SkillsData.js

--- src/data/SkillsData.js
+++ src/data/SkillsData.js
@@ -1,44 +1,86 @@
+import {
+    FaJava, FaJs, FaHtml5, FaReact, FaNodeJs, FaBootstrap, FaDocker, FaCss3Alt
+}
+    from 'react-icons/fa';
+
+import {
+    SiThymeleaf, SiSpringboot, SiMysql, SiMongodb,
+    SiPostgresql, SiSwagger, SiJunit5, SiHeroku, SiNetlify
+    , SiRailway, SiPostman, SiJirasoftware
+} from "react-icons/si";
+
+import { DiScrum } from "react-icons/di";
+
 const SkillsData = [
     {
         id: 1,
-        skillName: "Languages",
-        listTechnologies: ["Java","Java Script", "HTML", "CSS"]
+        skillName: 'Fundamentals',
+        listTechnologies: [
+            { name: 'Java', icon: <FaJava /> },
+            { name: 'JavaScript', icon: <FaJs /> },
+            { name: 'HTML', icon: <FaHtml5 /> },
+            { name: 'CSS', icon: <FaCss3Alt /> },
+        ],
     },
     {
         id: 2,
-        skillName: "Frontend Frameworks and Libraries",
-        listTechnologies: ["React.js", "Thymeleaf", "Bootstrap"]
+        skillName: 'Frontend',
+        listTechnologies: [
+            { name: 'React.js', icon: <FaReact /> },
+            { name: 'Thymeleaf', icon: <SiThymeleaf /> }, 
+            { name: 'Bootstrap', icon: <FaBootstrap /> },
+        ],
     },
     {
         id: 3,
-        skillName: "Backend Frameworks and Libraries",
-        listTechnologies: ["Node.js", "Spring Boot"]
+        skillName: 'Backend',
+        listTechnologies: [
+            { name: 'Node.js', icon: <FaNodeJs /> },
+            { name: 'Spring Boot', icon: <SiSpringboot /> },
+        ],
     },
     {
         id: 4,
-        skillName: "Database",
-        listTechnologies: ["H2 Database", "MySQL", "PostgreSQL", "MongoDB", "SQLLite", "Firebird"]
+        skillName: 'Databases',
+        listTechnologies: [
+            { name: 'MySQL', icon: <SiMysql /> },
+            { name: 'PostgreSQL', icon: <SiPostgresql /> },
+            { name: 'MongoDB', icon: <SiMongodb /> },
+        ],
     },
     {
         id: 5,
-        skillName: "Tests",
-        listTechnologies: ["JUnit", "Postman", "Swagger"]
+        skillName: 'Testing',
+        listTechnologies: [
+            { name: 'JUnit', icon: <SiJunit5 /> },
+            { name: 'Postman', icon: <SiPostman /> },
+            { name: 'Swagger', icon: <SiSwagger /> },
+        ],
     },
     {
         id: 6,
-        skillName: "Deploy",
-        listTechnologies: ["Heroku", "Netlify", "Railway", "Render"]
+        skillName: 'Deployment',
+        listTechnologies: [
+            { name: 'Heroku', icon: <SiHeroku /> },
+            { name: 'Netlify', icon: <SiNetlify /> },
+            { name: 'Railway', icon: <SiRailway /> },
+        ],
     },
     {
         id: 7,
-        skillName: "Containerization",
-        listTechnologies: ["Docker"]
+        skillName: 'DevOps',
+        listTechnologies: [
+            { name: 'Docker', icon: <FaDocker /> },
+        ],
     },
     {
         id: 8,
-        skillName: "Agile Methodologies",
-        listTechnologies: ["Scrum", "Jira"]
-    }
+        skillName: 'Agile',
+        listTechnologies: [
+            { name: 'Scrum', icon: <DiScrum /> },
+            { name: 'Jira', icon: <SiJirasoftware /> },
+        ],
+    },
 ];
 
 export default SkillsData;
