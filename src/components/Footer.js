abbdc0210e0d12131c9b5320f181ff2972c0543f
Jonas Machado <jonasmachado.ti@gmail.com>
2024-01-23 17:41:08 -0300

    :recycle: refactor: Improve Footer Styling
    - add 'footer-content' class to each div in Footer component;
    - create and style 'footer-content' class in footer.cs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,56 +1,65 @@
 import logo from "../assets/img/logo.png";
 import "../styles/footer.css";
-import { FaLinkedinIn } from 'react-icons/fa';
-import { AiFillInstagram, AiFillGithub } from 'react-icons/ai';
+import { FaLinkedinIn } from "react-icons/fa";
+import { AiFillInstagram, AiFillGithub } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-
-      <div>
+      <div className="footer-content">
         <h2>EXPLORE</h2>
-        <p>
-          <a href="/#">Homepage</a>
-        </p>
-        <p>
-          <a href="/#about">About</a>
-        </p>
-        <p>
-          <a href="/#skills">Skills</a>
-        </p>
-        <p>
-          <a href="/#project">Projects</a>
-        </p>
-        <p>
-          <a href="/contact">Contact</a>
-        </p>
+
+        <a href="/#">Homepage</a>
+
+        <a href="/#about">About</a>
+
+        <a href="/#skills">Skills</a>
+
+        <a href="/#project">Projects</a>
+
+        <a href="/contact">Contact</a>
       </div>
 
-      <div>
+      <div className="footer-content">
         <h2>INFORMAÇOES DE CONTATO</h2>
-        <p> <b> Whatsapp:</b> (35) 99912-8326</p>
-        <p> <b> email: </b>jonasmachado.ti@gmail.com</p>
+
+        <p>
+          {" "}
+          <b> Whatsapp:</b> (35) 99912-8326
+        </p>
+
+        <p>
+          {" "}
+          <b>Eemail: </b>jonasmachado.ti@gmail.com
+        </p>
+
         <div className="footer-social-icon">
           <a href="https://github.com/jonasmachados">
-            <i><AiFillGithub /></i>
+            <i>
+              <AiFillGithub />
+            </i>
           </a>
+
           <a href="https://www.instagram.com/jonasmachado01/">
-            <i> <AiFillInstagram /></i>
+            <i>
+              {" "}
+              <AiFillInstagram />
+            </i>
           </a>
+
           <a href="https://www.linkedin.com/in/jonas-machados/">
-            <i><FaLinkedinIn /></i>
+            <i>
+              <FaLinkedinIn />
+            </i>
           </a>
         </div>
       </div>
 
-      <div>
-        <img
-          src={logo}
-          alt="Logo"
-          className="footer-logo" />
+      <div className="footer-content">
+        <img src={logo} alt="Logo" className="footer-logo" />
+
         <h2>&copy; 2023 Jonas Machado. All rights reserved.</h2>
       </div>
-
     </div>
   );
 };
